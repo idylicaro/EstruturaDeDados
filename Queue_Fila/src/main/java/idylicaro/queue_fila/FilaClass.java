48b86a1657b36e1af02f872dae5b34c7821809c2
Idyl Icaro dos Santos <45442467+idylicaro@users.noreply.github.com>
2020-07-20 14:59:07 -0300

    Update FilaClass.java

--- Queue_Fila/src/main/java/idylicaro/queue_fila/FilaClass.java
+++ Queue_Fila/src/main/java/idylicaro/queue_fila/FilaClass.java
@@ -38,7 +38,7 @@ public class FilaClass {
         nItems--;
         return auxTemp;
     }
-     
+   
     public int viewFront(){
         return array[frente];
     }
@@ -52,8 +52,4 @@ public class FilaClass {
         return nItems;
     }
 
-    void remove(int i) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
